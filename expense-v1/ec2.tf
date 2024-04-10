645b2a0fb9c08c283b8f3d1362656eec0efbe001
chaitu <chaitus.ch09@gmail.com>
2024-04-10 16:34:53 -0700

    creating 3 aws instances

--- expense-v1/ec2.tf
+++ expense-v1/ec2.tf
@@ -1,28 +1,31 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
   tags=
   {
    Name= "frontend"
   }
-  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
+
 
 }
 resource "aws_instance" "backend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
   tags=
   {
     Name= "backend"
   }
-  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
+
 }
 resource "aws_instance" "mysql" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
   tags=
   {
     Name= "mysql"
   }
-  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
+
 }
