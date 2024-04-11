e0d9ced40d51478f7274d72223ae84b8b3305006
chaitu <chaitus.ch09@gmail.com>
2024-04-10 17:12:54 -0700

    creating a variables and retriving the variables

--- 03-variables/var.tf
+++ 03-variables/var.tf
@@ -6,10 +6,30 @@ variable "x" {
 # declaring a variable without a value
 variable "y"{}
 
+variable "y_list"{
+  default = [1,2,3]
+}
+
 # print the variable
+
+variable "z_map"{
+  default =
+  {
+    x=10
+    y=20
+  }
+}
 
 output "x" {
   value=var.x
 }
 
+output "y" {
+  value = var.y_list
+}
+
+output "z"
+{
+  value=var.z_map
+}
 
