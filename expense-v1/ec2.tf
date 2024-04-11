988fc19e9cebd83f8e0601147b6b47055c77b4d3
chaitu <chaitus.ch09@gmail.com>
2024-04-11 14:20:49 -0700

    creating a variables and retriving the variables

--- expense-v1/ec2.tf
+++ expense-v1/ec2.tf
@@ -18,11 +18,28 @@ resource "aws_instance" "backend" {
 
 }
 resource "aws_instance" "mysql" {
-  ami           = "ami-090252cbe067a9e58"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0c06371b05b6070a7"]
+  ami           = var.ami
+#  "ami-090252cbe067a9e58"
+  instance_type = var.instance_type
+#  "t3.micro"
+  vpc_security_group_ids = var.vpc_security_group_ids
+#  ["sg-0c06371b05b6070a7"]
   tags={
     Name= "mysql"
   }
+
+}
+
+variable "ami"{
+  default="ami-090252cbe067a9e58"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0c06371b05b6070a7"]
+
 
 }
