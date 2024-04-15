573ff96538b6906c4e21ef8368559fd2f4f4744a
chaitu <chaitus.ch09@gmail.com>
2024-04-15 14:35:06 -0700

    provisioners...remote-exec

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test" {
-  ami                    = "ami-05f020f5935e52dc4"
+  ami                    = "ami-090252cbe067a9e58"
   instance_type          = "t3.small"
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
